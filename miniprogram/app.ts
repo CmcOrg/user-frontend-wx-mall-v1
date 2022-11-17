41faab6d0e8a3503743564b3ce9ba2f065d71108
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-17 20:08:54 +0800

    【i】【CmcOrg】

---
+++ miniprogram/app.ts
@@ -0,0 +1,23 @@
+// app.ts
+App<IAppOption>({
+    globalData: {
+        statusBarHeight: 0,
+        menuButtonHeight: 0,
+        menuButtonWidth: 0,
+        menuButtoMarginTop: 0,
+        menuButtoMarginRight: 0,
+    },
+    onLaunch() {
+        const menuButton = wx.getMenuButtonBoundingClientRect();
+        const that = this
+        wx.getSystemInfo({
+            success: function (res) {
+                that.globalData.statusBarHeight = res.statusBarHeight
+                that.globalData.menuButtoMarginTop = menuButton.top - res.statusBarHeight
+                that.globalData.menuButtoMarginRight = res.windowWidth - menuButton.right
+                that.globalData.menuButtonWidth = menuButton.width + that.globalData.menuButtoMarginRight
+                that.globalData.menuButtonHeight = menuButton.height + (that.globalData.menuButtoMarginTop * 2);
+            },
+        })
+    },
+})
