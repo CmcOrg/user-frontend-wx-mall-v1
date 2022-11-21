16d10ed90368ee8d3aa358f4f1d68f68bb10553a
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-21 21:28:59 +0800

    【i】【CmcOrg】

--- miniprogram/page/index/index.ts
+++ miniprogram/page/index/index.ts
@@ -7,56 +7,64 @@ import PathConstant from "../../model/constant/PathConstant";
 
 const app = getApp<IAppOption>()
 
+type TOrderCardListItemOrderType = 'pay' | 'package' | 'comment' | 'exchange'
+
 interface IOrderCardListItem {
     icon: string
     title: string
     number: number
-    iconClass: string
+    orderType: TOrderCardListItemOrderType
 }
 
 interface IIndex {
+    statusBarHeight: string
+    menuButtonHeight: string
+    menuButtonMarginRight: number
     marquee: DrawMarquee
+    popupVisible: boolean
     userSelfInfoVO: UserSelfInfoVO // 登录的用户信息
     orderCardList: IOrderCardListItem[] // 我的订单 cardList
 }
 
-Page({
-    data: {
-        statusBarHeight: app.globalData.statusBarHeight + 'px',
-        menuButtonHeight: app.globalData.menuButtonHeight + 'px',
-        menuButtonMarginRight: app.globalData.menuButtonMarginRight,
-        marquee: {
-            speed: 10
+const data: IIndex = {
+    statusBarHeight: app.globalData.statusBarHeight + 'px',
+    menuButtonHeight: app.globalData.menuButtonHeight + 'px',
+    menuButtonMarginRight: app.globalData.menuButtonMarginRight,
+    marquee: {
+        speed: 10
+    },
+    popupVisible: false,
+    userSelfInfoVO: {} as UserSelfInfoVO,
+    orderCardList: [
+        {
+            icon: "wallet",
+            title: "待付款",
+            number: 0,
+            orderType: 'pay',
         },
-        popupVisible: false,
-        userSelfInfoVO: {} as UserSelfInfoVO,
-        orderCardList: [
-            {
-                icon: "wallet",
-                title: "待付款",
-                number: 0,
-                iconClass: 'f-23',
-            },
-            {
-                icon: "package",
-                title: "待收货",
-                number: 0,
-                iconClass: 'f-23',
-            },
-            {
-                icon: "comment",
-                title: "待评价",
-                number: 0,
-                iconClass: 'f-22',
-            },
-            {
-                icon: "exchang",
-                title: "退款/售后",
-                number: 0,
-                iconClass: 'f-22',
-            },
-        ]
-    } as IIndex,
+        {
+            icon: "package",
+            title: "待收货",
+            number: 0,
+            orderType: 'package',
+        },
+        {
+            icon: "comment",
+            title: "待评价",
+            number: 0,
+            orderType: 'comment',
+        },
+        {
+            icon: "exchang",
+            title: "退款/售后",
+            number: 0,
+            orderType: 'exchange',
+        },
+    ]
+}
+
+Page({
+    data,
     onLoad() {
         // 获取：用户基本信息
         GetUserInfo().then(res => {
@@ -64,6 +72,22 @@ Page({
                 userSelfInfoVO: res
             })
         })
+    },
+    orderItemClick(e: { currentTarget: { dataset: { orderType: TOrderCardListItemOrderType; }; }; }) {
+        const {orderType} = e.currentTarget.dataset;
+        if (orderType === 'pay') { // 待支付
+            console.log("待支付")
+            InDev()
+        } else if (orderType === 'package') { // 待收货
+            console.log("待收货")
+            InDev()
+        } else if (orderType === 'comment') { // 待评价
+            console.log("待评价")
+            InDev()
+        } else if (orderType === 'exchange') { // 退款/售后
+            console.log("退款/售后")
+            InDev()
+        }
     },
     InDev() {
         InDev()
