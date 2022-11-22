8e7c094db3ea78a82537751d6d1c1436b51b1240
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-23 01:28:33 +0800

    【i】【CmcOrg】

--- miniprogram/packageA/page/dinner/dinner.ts
+++ miniprogram/packageA/page/dinner/dinner.ts
@@ -15,12 +15,14 @@ interface ICategoryListItem {
 interface IDinner {
     sideBarIndex: number
     scrollTop: number
+    offsetTopList: number[]
     categoryList: ICategoryListItem[]
 }
 
 const data: IDinner = {
     sideBarIndex: 0,
     scrollTop: 0,
+    offsetTopList: [],
     categoryList: [
         {
             label: '波波奶茶',
@@ -51,24 +53,38 @@ const data: IDinner = {
 }
 
 Page({
-    offsetTopList: [],
+    // [0, 1941, 3882, 5823, 7764]
+    // 9905
+    itemTitleHeight: 61,
+    itemProductHeight: 75,
+    itemProductGapHeight: 20, // 间隔高度
+    allHeight: 0, // 整体高度
+    marginBottomHeight: 200, // 距离底部的高度
     data,
     onLoad() {
-        this.imageLoad()
+        this.calcAllHeight() // 计算：整体高度
+
     },
-    imageLoad() {
-        const query = wx.createSelectorQuery().in(this);
-        query
-            .selectAll('.title')
-            .boundingClientRect((rectList: any) => {
-                this.offsetTopList = rectList.map((item: { top: number; }) => item.top);
-            }).exec();
+    // 计算：整体高度
+    calcAllHeight() {
+
+        this.data.categoryList.forEach((item, index) => {
+            let itemAllHeight = 0 // 这一块分类商品的整体高度
+            itemAllHeight += this.itemTitleHeight
+            itemAllHeight += item.itemList.length * this.itemProductHeight
+            itemAllHeight += (item.itemList.length - 1) * this.itemProductGapHeight
+            this.data.offsetTopList[index] = this.allHeight
+            this.allHeight += itemAllHeight
+        })
+
+        this.allHeight += this.marginBottomHeight
+
     },
     onSideBarChange(e: { detail: { value: number; }; }) {
 
         const {value} = e.detail;
 
-        this.setData({sideBarIndex: value, scrollTop: this.offsetTopList[value]});
+        this.setData({sideBarIndex: value, scrollTop: this.data.offsetTopList[value]});
 
     },
     onScroll(e: { detail: { scrollTop: number; }; }) {
@@ -82,7 +98,7 @@ Page({
             return;
         }
 
-        const index = this.offsetTopList.findIndex((item) => item > scrollTop && item - scrollTop <= threshold);
+        const index = this.data.offsetTopList.findIndex((item) => item > scrollTop && item - scrollTop <= threshold);
 
         if (index !== -1 && this.data.sideBarIndex !== index) {
             this.setData({sideBarIndex: index});
