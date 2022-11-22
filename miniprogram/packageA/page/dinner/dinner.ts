f12b594dd0d01eb5f1fbe1da4f1724ae1ad14ef4
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-23 01:04:43 +0800

    【i】【CmcOrg】

--- miniprogram/packageA/page/dinner/dinner.ts
+++ miniprogram/packageA/page/dinner/dinner.ts
@@ -1,45 +1,63 @@
 const image = 'https://tdesign.gtimg.com/mobile/%E5%9B%BE%E7%89%87.png';
 const itemList = new Array(20).fill({label: '标题文字', image}, 0, 20);
 
+interface ICategoryListItemItemListItem {
+    label: string
+    image: string
+}
+
+interface ICategoryListItem {
+    label: string,
+    title: string,
+    itemList: ICategoryListItemItemListItem[],
+}
+
+interface IDinner {
+    sideBarIndex: number
+    scrollTop: number
+    categoryList: ICategoryListItem[]
+}
+
+const data: IDinner = {
+    sideBarIndex: 0,
+    scrollTop: 0,
+    categoryList: [
+        {
+            label: '波波奶茶',
+            title: '标题',
+            itemList,
+        },
+        {
+            label: '普通奶茶',
+            title: '标题',
+            itemList,
+        },
+        {
+            label: '果茶',
+            title: '标题',
+            itemList,
+        },
+        {
+            label: '烧仙草',
+            title: '标题',
+            itemList,
+        },
+        {
+            label: '芋圆奶茶',
+            title: '标题',
+            itemList,
+        },
+    ],
+}
+
 Page({
     offsetTopList: [],
-    data: {
-        sideBarIndex: 0,
-        scrollTop: 0,
-        categoryList: [
-            {
-                label: '选项选项选项',
-                title: '标题',
-                itemList,
-            },
-            {
-                label: '选项',
-                title: '标题',
-                itemList,
-            },
-            {
-                label: '选项',
-                title: '标题',
-                itemList,
-            },
-            {
-                label: '选项',
-                title: '标题',
-                itemList,
-            },
-            {
-                label: '选项',
-                title: '标题',
-                itemList,
-            },
-        ],
-    },
+    data,
     onLoad() {
         this.imageLoad()
     },
     imageLoad() {
         const query = wx.createSelectorQuery().in(this);
-
         query
             .selectAll('.title')
             .boundingClientRect((rectList: any) => {
