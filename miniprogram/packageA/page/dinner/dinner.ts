6f2246cf15e278e6a91856c2a9f3c57276c17512
法外狂徒李四 <dimensional_logic@qq.com>
2022-12-03 19:01:37 +0800

    【i】【CmcOrg】

--- miniprogram/packageA/page/dinner/dinner.ts
+++ miniprogram/packageA/page/dinner/dinner.ts
@@ -19,6 +19,7 @@ Page({
     data,
     onLoad() {
         this.getListDate() // 获取列表数据
+        this.offsetTopListInit()
     },
     // 获取列表数据
     getListDate() {
@@ -28,30 +29,36 @@ Page({
         TakeawaySpuUserProduct({scene: 1}).then(res => {
             this.setData({
                 productList: res.data
+            }, () => {
+                this.offsetTopListInit()
             })
             wx.setStorageSync(LocalStorageKey.DINNER_SPU_USER_PRODUCT, res.data)
         })
     },
+    offsetTopListInit() {
+        const query = wx.createSelectorQuery().in(this);
+        query
+            .selectAll('.title')
+            .boundingClientRect((rectList: any) => {
+                const offsetTopList = rectList.map((item: { top: number; }) => item.top);
+                this.setData({
+                    offsetTopList
+                })
+            }).exec();
+    },
     onSideBarChange(e: { detail: { value: number; }; }) {
-
         const {value} = e.detail;
-
         this.setData({sideBarIndex: value, scrollTop: this.data.offsetTopList[value]});
-
     },
     onScroll(e: { detail: { scrollTop: number; }; }) {
 
         const {scrollTop} = e.detail;
-
         const threshold = 50; // 下一个标题与顶部的距离
-
         if (scrollTop < threshold) {
             this.setData({sideBarIndex: 0});
             return;
         }
-
         const index = this.data.offsetTopList.findIndex((item) => item > scrollTop && item - scrollTop <= threshold);
-
         if (index !== -1 && this.data.sideBarIndex !== index) {
             this.setData({sideBarIndex: index});
         }
